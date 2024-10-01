d80180337fb24d27fefee7b9e0aeeef352d00d74
fulfix <fulfix.sellier@gmail.com>
2024-10-01 18:32:54 +0200

    Initial Commit

---
+++ paler1nGUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  paler1nGUI
+//
+//  Created by denis on 01/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ paler1nGUI/paler1nGUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  paler1nGUIApp.swift
+//  paler1nGUI
+//
+//  Created by denis on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct paler1nGUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
