de3fbe4aa3d0e6d378c423c8e9bb271b08546c9e
fulfix <fulfix.sellier@gmail.com>
2024-10-01 18:32:54 +0200

    Initial Commit

---
+++ paler1nGUITests/paler1nGUITests.swift
@@ -0,0 +1,17 @@
+//
+//  paler1nGUITests.swift
+//  paler1nGUITests
+//
+//  Created by denis on 01/10/2024.
+//
+
+import Testing
+@testable import paler1nGUI
+
+struct paler1nGUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
